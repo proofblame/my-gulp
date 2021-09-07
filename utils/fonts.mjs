b205f54fd3630fd1a1d5bb628f117d3862f8beb1
Timur Marokko <proofblame@gmail.com>
2021-09-07 13:16:02 +0300

    Собрать fonts

--- utils/fonts.mjs
+++ utils/fonts.mjs
@@ -1,21 +1,37 @@
+// Modules
+import {
+  path,
+  sourceFolder,
+} from './path.mjs';
+import pkg from 'gulp';
+import fileSystem from 'fs';
+import browserSync from 'browser-sync';
+
+// Const
+const {
+  src
+} = pkg;
+
 function fonts() {
-	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
-	if (file_content == '') {
-		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
-		fs.readdir(path.build.fonts, function (err, items) {
-			if (items) {
-				let c_fontname;
-				for (var i = 0; i < items.length; i++) {
-					let fontname = items[i].split('.');
-					fontname = fontname[0];
-					if (c_fontname != fontname) {
-						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-					}
-					c_fontname = fontname;
-				}
-			}
-		})
-	}
-	return src(path.src.html).pipe(browsersync.stream());
+  const fileContent = fileSystem.readFileSync(`${sourceFolder}/scss/fonts.scss`);
+  if (fileContent == '') {
+    fileSystem.writeFile(`${sourceFolder}/scss/fonts.scss`, '', callback);
+    fileSystem.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (let i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.');
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fileSystem.appendFile(`${sourceFolder}/scss/fonts.scss`, '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', callback);
+          }
+          c_fontname = fontname;
+        }
+      }
+    })
+  }
+  return src(path.src.html).pipe(browserSync.stream());
 }
+
+function callback() {}
 export default fonts;
