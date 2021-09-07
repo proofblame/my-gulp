664032c7af283472c684b9161d3e6c4b1a05fc7b
Timur Marokko <proofblame@gmail.com>
2021-09-07 11:59:18 +0300

    Собрать images

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -8,6 +8,7 @@ import  browsersync from './utils/browsersync.mjs';
 import  html from './utils/html.mjs';
 import  css from './utils/css.mjs';
 import  script from './utils/script.mjs';
+import  images from './utils/images.mjs';
 
 
 
@@ -16,5 +17,6 @@ export {
   browsersync,
   html,
   css,
-  script
+  script,
+  images
 }
---
+++ utils/images.mjs
@@ -0,0 +1,53 @@
+// Plugins
+import pkg          from 'gulp';
+import browserSync  from 'browser-sync';
+import newer        from 'gulp-newer';
+import rename       from 'gulp-rename';
+import imagemin     from 'gulp-imagemin';
+import webp         from 'imagemin-webp';
+
+// Modules
+import {
+  path
+}                    from './path.mjs';
+
+// Const
+const {
+  dest,
+  src
+} = pkg;
+
+// Task
+const images = () => {
+  return src(path.src.img)
+    .pipe(newer(path.build.img))
+    .pipe(
+      imagemin([
+        webp({
+          quality: 75
+        })
+      ])
+    )
+    .pipe(
+      rename({
+        extname: ".webp"
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(newer(path.build.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{
+          removeViewBox: false
+        }],
+        interlaced: true,
+        optimizationLevel: 3 // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browserSync.stream())
+}
+
+export default images;
